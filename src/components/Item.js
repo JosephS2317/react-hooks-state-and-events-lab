3b0c6c0d133090ae2fe23b177535666447108587
Joseph <JosephSped23@gmail.com>
2024-04-18 14:15:06 -0400

    passed tests for state and events

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [ cart, setCart ]= useState(true)
+  function cartHandler (){
+    setCart(!cart)
+  }
+    const buttonText = cart ? "Add to Cart" : "Remove From Cart"
+    const cartClass = cart ? "" : "in-cart"
+   
   return (
-    <li className="">
+    <li className={cartClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick= {cartHandler} className="add">{buttonText}</button>
     </li>
   );
 }
+
+// function Item({ name, category }) {
+
+//   const [ cart, cartSetter ] = useState(true)
+//   function cartHandler (){
+//     cartSetter(!cart)
+
+//   }
+//   const buttContext = cart ? "Add to Cart" : "Remove From Cart"
+//   const cartClass = cart ? "" : "in-cart"
+ 
+//   return (
+//     <li className={cartClass}>
+//       <span>{name}</span>
+//       <span className="category">{category}</span>
+//       <button onClick={cartHandler}className="add">{buttContext}</button>
+//     </li>
+//   );
+// }
+
 
 export default Item;
